d71cdabcc5cdf9e51b09f8ac286b32c6260cdc02
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2020-11-12 10:14:10 -0700

    update CORS policy to allow http://localhost:1234

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ require('./passport');
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = [
+    'http://localhost:8080', 
+    'http://testsite.com', 
+    'http://localhost:1234'
+];
 app.use(cors({
     origin: (origin, callback) => {
         if(!origin) return callback(null, true);
