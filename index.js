9051970864eb3b2264dcde94d95afbb65642445e
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2020-11-23 12:23:50 -0700

    update CORS policy

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let allowedOrigins = [
     'http://localhost:8080', 
     'http://testsite.com', 
     'http://localhost:1234',
-    'https://amazing-neumann-2a043b.netlify.app/'
+    'https://flixnet2020.netlify.app/'
 ];
 app.use(cors({
     origin: (origin, callback) => {
