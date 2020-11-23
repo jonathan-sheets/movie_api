58b25fdcc4758fe378b78d501d850033cea005ec
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2020-11-23 15:12:22 -0700

    update CORS

--- index.js
+++ index.js
@@ -17,24 +17,24 @@ require('./passport');
 
 const { check, validationResult } = require('express-validator');
 
-// const cors = require('cors');
-// let allowedOrigins = [
-//     'http://localhost:8080', 
-//     'http://testsite.com', 
-//     'http://localhost:1234',
-//     'https://flixnet-2020.herokuapp.com',
-//     'https://flixnet2020.netlify.app/'
-// ];
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         if(!origin) return callback(null, true);
-//         if(allowedOrigins.indexOf(origin) === -1) { //if a specific origin ins't found on the list of allowed origins
-//             let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-//         return callback(new Error(message ), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
+const cors = require('cors');
+let allowedOrigins = [
+    'http://localhost:8080', 
+    'http://testsite.com', 
+    'http://localhost:1234',
+    'https://flixnet-2020.herokuapp.com',
+    'https://flixnet2020.netlify.app/'
+];
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1) { //if a specific origin ins't found on the list of allowed origins
+            let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+        return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 app.use(bodyParser.json());
 
